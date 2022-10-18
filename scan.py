fd4e103e2cf22446ac359140debf42cb44095d7c
jinglingXu <xujingling0927@icloud.com>
2022-10-18 17:25:11 +0800

    modify:README.md

--- scan.py
+++ scan.py
@@ -2,14 +2,14 @@
 import re
 from os import listdir
 from PIL import Image
-
+from tqdm import tqdm
 def scan_dir(dir):
     
     #获取文件夹内所有图片
     image_list = []
-    for file_name in listdir(dir):
+    dirs = listdir(dir)
+    for file_name in tqdm(dirs):
         if bool(re.search(r'\.(png|jpg|jpeg)$', file_name, re.IGNORECASE)):
-            print(f'扫描照片: {file_name}')
             image_list.append(Image.open(f'{dir}/{file_name}'))
 
     total = len(image_list)
